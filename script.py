10ee625f354cc9f227fb17490aa9a1b3742bec86
John Gallo <jgallo@cpg.org>
2023-02-11 08:26:06 -0500

    Tables completed

--- script.py
+++ script.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 
-# Importing the Yattag library
+'''
+    @author: John Gallo
+
+    Boxnotes Converter
+
+    Usage: 
+
+'''
+
 from yattag import Doc
 import json
+import logging
 
-# filename = "its-operations - 1-10-2023.boxnote"
-filename = "Newsletter1.boxnote"
-
-# Open boxfile for reading
-with open(filename, "r") as boxnote:
-    data = json.load(boxnote)
-
-# object -> doc -> content
-content_list = data['doc']['content']
-
-d21 = content_list[21]
-
-def print_content(el,html_style=''):
-    # Recursive procedure to go through nested structure
+''' Convert Boxnote to HTML '''
+def content_to_html(el,html_style=''):
+    # Recursively go through nested structure
     if 'content' in el.keys():
         if el['type'] == 'bullet_list':
             with tag('ul'):
                 for item in el['content']:
-                    print_content(item)
+                    content_to_html(item)
+
         elif el['type'] == 'list_item':
             with tag('li'):
                 for item in el['content']:
-                    print_content(item)
+                    content_to_html(item)
+
         elif el['type'] == 'paragraph':
             with tag('p'):
                 if 'marks' in el.keys():
@@ -34,12 +34,37 @@ def print_content(el,html_style=''):
                         if 'attrs' in mark.keys():
                             if 'alignment' in mark['attrs']:
                                 # doc.attr(style = "text-align:{};".format(mark['attrs']['alignment']))
-                                html_style += "text-align:{};".format(mark['attrs']['alignment'])
-                                 
+                                html_style += "text-align:{};".format(mark['attrs']['alignment'])             
                 for item in el['content']:
-                    print_content(item, html_style)
+                    content_to_html(item, html_style)
 
-        else: print("*** Unsupported type (in-content): {}".format(el['type']))
+        elif el['type'] == 'table':
+            with tag('table', cellspacing="0", border="1"):
+                for item in el['content']:
+                    content_to_html(item)
+        
+        elif el['type'] == 'table_row':
+            with tag('tr'):
+                for item in el['content']:
+                    content_to_html(item)
+        
+        elif el['type'] == 'table_cell':
+        
+            try:
+                t_colspan = el['attrs']['colspan']
+                t_rowspan = el['attrs']['rowspan']
+                t_colwidth = el['attrs']['colwidth'][0]
+
+                with tag('td', colspan="{}".format(t_colspan), rowspan="{}".format(t_rowspan), colwidth="{}".format(t_colwidth) ):
+                    for item in el['content']:
+                        content_to_html(item)
+
+            except (KeyError, TypeError):
+                with tag('td'):
+                    for item in el['content']:
+                        content_to_html(item)
+
+        else: logging.warning("*** Unsupported type (in-content): {}".format(el['type']))
     
     # HTML convertible line(s)
     elif 'text' in el.keys():
@@ -57,7 +82,7 @@ def print_content(el,html_style=''):
                 elif mark['type'] == 'strong':
                     html_strong = True
                 else:
-                    print("MARK: Unsupported type: {} - {}".format(el['type'],mark['type']))
+                    logging.warning("*** Unsupported type (in-text): {} - {}".format(el['type'],mark['type']))
 
             if html_style:
                 doc.attr(style = html_style)
@@ -71,65 +96,37 @@ def print_content(el,html_style=''):
                 
     elif el['type'] == 'paragraph' and len(el.keys()) > 1:
         if 'marks' not in el.keys(): 
-            print("*** Unsupported type: {}".format(el['type']))
-            print(el)
+            logging.warning("*** Unsupported type: {}".format(el['type']))
+            logging.warning(el)
 
+
+if __name__ == '__main__':    
+    # Set logging configuration
+    # logging.basicConfig(format='%(asctime)s %(message)s')
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    # filename = "its-operations - 1-10-2023.boxnote"
+    # filename = "Newsletter1.boxnote"
+    filename = "Boxnote1.boxnote"
+
+    # Open Boxnote for reading
+    with open(filename, "r") as boxnote:
+        data = json.load(boxnote)
+
+    # Get interesting Node: object -> doc -> content
+    content_list = data['doc']['content']
+
+    # Initiate HTML Objects
+    doc, tag, text = Doc().tagtext()
+
+    # Generate HTML
+    with tag('html'):
+        with tag('body'):
+            # content_to_html(content_list[21])
+            for elm in content_list:
+                content_to_html(elm)
+
+    html_result = doc.getvalue()
     
+    print(html_result)
 
-doc, tag, text = Doc().tagtext()
-
-with tag('html'):
-    with tag('body'):
-        # print_content(content_list[21])
-        for elm in content_list:
-            print_content(elm)
-
-        # with tag('p', id = 'main'):
-        #     text('We can write any text here')
-        # with tag('a', href = '/my-link'):
-        #     text('We can insert any link here')
-        
-
-result = doc.getvalue()
-print(result)
-
-
-# data_sample = {'type': 'bullet_list',
-#  'content': [{'type': 'list_item',
-#    'content': [{'type': 'paragraph',
-#      'content': [{'type': 'text',
-#        'marks': [{'type': 'font_size', 'attrs': {'size': '0.8125em'}},
-#         {'type': 'strong'},
-#         {'type': 'underline'},
-#         {'type': 'author_id', 'attrs': {'authorId': '180701212'}}],
-#        'text': 'GC80 - 2022'}]}]},
-#   {'type': 'list_item',
-#    'content': [{'type': 'paragraph',
-#      'content': [{'type': 'text',
-#        'marks': [{'type': 'font_size', 'attrs': {'size': '0.8125em'}},
-#         {'type': 'strong'},
-#         {'type': 'underline'},
-#         {'type': 'author_id', 'attrs': {'authorId': '180701212'}}],
-#        'text': 'Software Asset Management'}]}]},
-#   {'type': 'list_item',
-#    'content': [{'type': 'paragraph',
-#      'content': [{'type': 'text',
-#        'marks': [{'type': 'font_size', 'attrs': {'size': '0.8125em'}},
-#         {'type': 'strong'},
-#         {'type': 'author_id', 'attrs': {'authorId': '180701212'}}],
-#        'text': 'ITS - Ops – 2021 Applications Migration to the Cloud'}]}]},
-#   {'type': 'bullet_list',
-#    'content': [{'type': 'list_item',
-#      'content': [{'type': 'paragraph',
-#        'content': [{'type': 'text',
-#          'marks': [{'type': 'font_size', 'attrs': {'size': '0.8125em'}},
-#           {'type': 'strong'},
-#           {'type': 'author_id', 'attrs': {'authorId': '180701212'}}],
-#          'text': 'SecureTransport Migration'}]}]},
-#     {'type': 'list_item',
-#      'content': [{'type': 'paragraph',
-#        'content': [{'type': 'text',
-#          'marks': [{'type': 'font_size', 'attrs': {'size': '0.8125em'}},
-#           {'type': 'strong'},
-#           {'type': 'author_id', 'attrs': {'authorId': '180701212'}}],
-#          'text': 'IR to containers | Axcelinno working on Gitlab upgrade\\'}]}]}]}]}
