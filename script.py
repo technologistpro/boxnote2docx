0d87b63d4730e030e3599faab9991c56845fd36d
John Gallo <jgallo@cpg.org>
2023-02-15 10:12:04 -0500

    Create HTML and Docx documents

--- script.py
+++ script.py
@@ -10,8 +10,10 @@
 '''
 
 from yattag import Doc
+from htmldocx import HtmlToDocx
 import json
 import logging
+
 
 ''' Convert Boxnote to HTML '''
 def content_to_html(el,html_style=''):
@@ -38,6 +40,22 @@ def content_to_html(el,html_style=''):
                 for item in el['content']:
                     content_to_html(item, html_style)
 
+        elif el['type'] == 'code_block':
+            with tag('code'):
+                for item in el['content']:
+                    content_to_html(item)
+
+        elif el['type'] == 'blockquote':
+            with tag('blockquote'):
+                for item in el['content']:
+                    content_to_html(item)
+
+        elif el['type'] == 'call_out_box':
+            with tag('blockquote'):
+                # html_style += "padding: 5px; background-color: #E1E8F2; border: dotted 2px #00518A;"
+                for item in el['content']:
+                    content_to_html(item, html_style)
+                    
         elif el['type'] == 'table':
             with tag('table', cellspacing="0", border="1"):
                 for item in el['content']:
@@ -71,6 +89,8 @@ def content_to_html(el,html_style=''):
         # Additional attributes 
         # html_style = ''
         html_strong = False
+        html_em = False
+        html_s = False
         if 'marks' in el.keys():
             for mark in el['marks']:                
                 if mark['type'] == 'font_color':
@@ -78,9 +98,15 @@ def content_to_html(el,html_style=''):
                 elif mark['type'] == 'font_size':
                     html_style+="font-size:{};".format(mark['attrs']['size'])
                 elif mark['type'] == 'underline':
-                    html_style+="text decoration:underline;"
+                    html_style+="text-decoration:underline;"
                 elif mark['type'] == 'strong':
                     html_strong = True
+                elif mark['type'] == 'em':
+                    html_em = True
+                elif mark['type'] == 'strikethrough':
+                    html_s = True
+                elif mark['type'] == 'author_id':
+                    pass
                 else:
                     logging.warning("*** Unsupported type (in-text): {} - {}".format(el['type'],mark['type']))
 
@@ -89,6 +115,12 @@ def content_to_html(el,html_style=''):
                
         if html_strong:
             with tag('strong'):  
+                text(el['text'])
+        elif html_em:
+            with tag('em'):  
+                text(el['text'])
+        elif html_s:
+            with tag('s'):  
                 text(el['text'])
         else:
             text(el['text'])
@@ -101,17 +133,20 @@ def content_to_html(el,html_style=''):
 
 
 if __name__ == '__main__':    
+    
     # Set logging configuration
-    # logging.basicConfig(format='%(asctime)s %(message)s')
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
     # filename = "its-operations - 1-10-2023.boxnote"
-    # filename = "Newsletter1.boxnote"
-    filename = "Boxnote1.boxnote"
+    filename = "Newsletter1.boxnote"
+    # filename = "Airtable: Client Stories App Requirements.boxnote"
 
     # Open Boxnote for reading
     with open(filename, "r") as boxnote:
         data = json.load(boxnote)
+
+    # Determine if object contains doc object or atext object
+    
 
     # Get interesting Node: object -> doc -> content
     content_list = data['doc']['content']
@@ -128,5 +163,16 @@ if __name__ == '__main__':
 
     html_result = doc.getvalue()
     
-    print(html_result)
+    # print(html_result)
+    with open(filename.replace('boxnote', "html"), "w") as file:
+        file.write(html_result)
+
+    new_parser = HtmlToDocx()
+    new_parser.table_style = 'TableGrid'
+    docx = new_parser.parse_html_string(html_result)
+    
+    docx.save(filename.replace('boxnote', "docx"))
+
+
+
 
