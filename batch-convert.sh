5473a811ab6f1c750ce29a0de8e5cc57208eeb59
John Gallo <jgallo@cpg.org>
2023-12-08 10:29:08 -0500

    updated batch-convert to use boxnotes source folder

--- batch-convert.sh
+++ batch-convert.sh
@@ -2,9 +2,9 @@ USER=$1
 
 mkdir converted/boxnotes-${USER}
 
-find ../boxsdk/boxnotes/${USER} -type f -name '*.boxnote' -print0 |                                        
+find ../get-boxnotes/boxnotes/${USER} -type f -name '*.boxnote' -print0 |                                        
 while IFS= read -r -d '' file; do
     ./boxnote2docx -b "$file" -d converted/boxnotes-${USER} 2>&1
 done | tee ${USER}.log
 
-cp ../boxsdk/boxnotes/${USER}/inventory.csv converted/boxnotes-${USER}/
+cp ../get-boxnotes/boxnotes/${USER}/inventory.csv converted/boxnotes-${USER}/
